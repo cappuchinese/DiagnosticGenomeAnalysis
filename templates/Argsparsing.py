ae7dd4a853ec11e2253635eba4f354d67ecac77c
Lisa Hu <ljbhu2002@gmail.com>
2021-11-03 12:54:17 +0100

    Added argsparse module

---
+++ templates/Argsparsing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Module to parse terminal arguments
+"""
+
+import argparse
+from getpass import getpass
+
+
+def _args_parsing():
+    """
+    Function to parse terminal commands
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", metavar="H", help="Host of database")
+    parser.add_argument("user", metavar="U", help="Name of the user")
+    parser.add_argument("database", metavar="D", help="Name of database")
+    parser.add_argument("-p", "password", action="store_true", dest="password",
+                        help="Password of the user")
+    parser.add_argument("anno_file", metavar="F", help="ANNOVAR file")
+    args = parser.parse_args()
+
+    if args.password:
+        password = getpass("Enter password: ")
+
+    return args, password
